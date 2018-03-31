c058fb40f221170a9606fcaa43fac574dbcbe9b4
rapirent <rapirent@gmail.com>
2018-04-01 02:43:18 +0800

    Use the Queue not the Manager (Server process)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Queue
 import sys
 import csv
 import re
@@ -8,15 +8,13 @@ RE_CJK = re.compile(r'[\u4e00-\ufaff]+', re.UNICODE)
 RE_ENG = re.compile(r'[a-zA-Z]+')
 RE_ALL = re.compile(r'[a-zA-Z\u4e00-\ufaff]+', re.UNICODE)
 
-PROCESS_NUM = 2
+PROCESS_NUM = 8
 
-class Trie():
-    pass
 
 def split_line(filename):
     csvfile = open(filename, 'r', newline='')
     sourcereader = csv.reader(csvfile, delimiter=',')
-    index = [list(), list()]
+    index = [list(), list(), list(), list(), list(), list(), list(), list()]
     for (i, line) in enumerate(sourcereader):
         cjk_strings = RE_CJK.findall(line[1])
         eng_strings = RE_ENG.findall(line[1])
@@ -35,20 +33,20 @@ def split_line(filename):
 def or_search(print_list, queries, index_string, process_index):
     for (i, search_line) in enumerate(index_string):
         if queries & search_line:
-            print_list.append(i*PROCESS_NUM+process_index+1)
+            print_list.put(i*PROCESS_NUM+process_index+1)
 
 
 def and_search(print_list, queries, index_string, process_index):
     for (i, search_line) in enumerate(index_string):
         if queries < search_line:
-            print_list.append(i*PROCESS_NUM+process_index+1)
+            print_list.put(i*PROCESS_NUM+process_index+1)
 
 
 def not_search(print_list, in_element, notin_element, index_string, process_index):
     for (i, search_line) in enumerate(index_string):
         if (in_element in search_line
             and not notin_element < search_line):
-            print_list.append(i*PROCESS_NUM+process_index+1)
+            print_list.put(i*PROCESS_NUM+process_index+1)
 
 
 if __name__ == '__main__':
@@ -73,38 +71,34 @@ if __name__ == '__main__':
         for query_line in q:
             query_line = re.sub('\n', '', query_line)
             # with Manager() as manager:
-            print_list = Manager().list()
-            processes = list()
+            print_list = Queue()
             if 'or' in query_line:
                 queries = set(re.split(' or ', query_line))
-                for i in range(0, PROCESS_NUM):
-                    p = Process(target=or_search,
-                                args=(print_list, queries, index_string[i], i))
-                    p.start()
-                    processes.append(p)
-
+                processes = [Process(target=or_search,
+                                     args=(print_list, queries, index_string[i], i))
+                             for i in range(0, PROCESS_NUM)]
             elif 'and' in query_line:
                 queries = set(re.split(' and ', query_line))
-                for i in range(0, PROCESS_NUM):
-                    p = Process(target=and_search,
-                                args=(print_list, queries, index_string[i], i))
-                    p.start()
-                    processes.append(p)
+                processes = [Process(target=and_search,
+                                     args=(print_list, queries, index_string[i], i))
+                             for i in range(0,PROCESS_NUM)]
             elif 'not' in query_line:
                 queries = re.split(' not ', query_line)
                 in_element = queries[0]
                 notin_element = set(queries[1:])
-                for i in range(0, PROCESS_NUM):
-                    p = Process(target=not_search,
-                                args=(print_list, in_element, notin_element, index_string[i], i))
-                    p.start()
-                    processes.append(p)
+                processes = [Process(target=not_search,
+                                     args=(print_list, in_element, notin_element, index_string[i], i))
+                             for i in range(0, PROCESS_NUM)]
+            else:
+                raise ValueError()
 
+            for process in processes:
+                process.start()
             for process in processes:
                 process.join()
 
-            if not print_list:
+            if print_list.empty():
                 print('0', file=o)
             else:
-                print_list.sort()
-                print(','.join(map(str, print_list)), file=o)
+                print_list.put(-1)
+                print(','.join(map(str, sorted([i for i in iter(print_list.get, -1)]))), file=o)
